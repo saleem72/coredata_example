83d189bd788649acb23360318f1cdf3c1b0d75e6
Nabil_saadi <nabil.saadi@codersdc.com>
2023-02-14 09:26:35 +0300

    Initial Commit

---
+++ coredata_example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coredata_example
+//
+//  Created by Yousef on 2/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coredata_example/coredata_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  coredata_exampleApp.swift
+//  coredata_example
+//
+//  Created by Yousef on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct coredata_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
